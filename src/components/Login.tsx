a7646e43daf1d5fe8a93bb2b95aae31f8c871724
itsarasib <itsara.pliansart@gmail.com>
2024-11-05 20:31:10 +0700

    feat:login

---
+++ src/components/Login.tsx
@@ -0,0 +1,16 @@
+const Login = () => {
+  return (
+    <div className="h-[calc(100vh-120px)] border flex justify-center items-center">
+      <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-6 mb-4">
+        <h2 className="text-xl font-semibold mb-4">Please Login</h2>
+        <form>
+          <div>
+            <label htmlFor=""></label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
