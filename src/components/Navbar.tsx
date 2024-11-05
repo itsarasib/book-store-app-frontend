b75944057136ed30888bcddc3d0d2d4b8299e23a
itsarasib <itsara.pliansart@gmail.com>
2024-11-05 19:14:48 +0700

    fix: log

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const navigation = [
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const currentUser = false;
+  const currentUser = true;
 
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
