1b75ecb13b0930019dc0ef909262d0e3851a485f
itsarasib <itsara.pliansart@gmail.com>
2024-11-23 19:50:00 +0700

    fix: getbaseurl

--- src/utills/baseUrl.ts
+++ src/utills/baseUrl.ts
@@ -1,10 +1,5 @@
 const getBaseUrl = () => {
-  // return "https://books-store-app-backend.vercel.app";
-  // return "http://localhost:3000";
-  console.log(import.meta.env.VITE_BASE_URL);
-  return import.meta.env.VITE_BASE_URL == "production"
-    ? "https://books-store-app-backend.vercel.app"
-    : "http://localhost:3000";
+  return "https://books-store-app-backend.vercel.app";
 };
 
 export default getBaseUrl;
